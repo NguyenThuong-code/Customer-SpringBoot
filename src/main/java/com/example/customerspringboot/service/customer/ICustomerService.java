9101d45d3b306cd12dbf65ad471feff01dee1f95
NguyenThuong-code <you@example.com>
2022-11-04 11:15:46 +0700

    Customer Spring

---
+++ src/main/java/com/example/customerspringboot/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.customerspringboot.service.customer;
+
+import com.example.customerspringboot.model.Customer;
+import com.example.customerspringboot.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
